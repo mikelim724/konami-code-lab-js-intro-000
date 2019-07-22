e7f1eacd8fcd86a700adc25267e98ab7107c9957
imperative-cron-3751 <mikelim724@gmail.com>
2019-07-22 04:38:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,25 @@ const codes = [
   "a"
 ];
 
+var index=0;
+
 function init() {
-  // your code here
+  const bodyinput = document.querySelector('body');
+  bodyinput.addEventListener('keydown', onKeyDownHandler);
+}
+
+function onKeyDownHandler(e) {
+  const key = e.key;
+
+  if (key===codes[index]) {
+    index++;
+
+    if (index===codes.length) {
+      alert("Hurray!");
+      index =0;
+    }
+  }
+  else {
+    index = 0;
+  }
 }
